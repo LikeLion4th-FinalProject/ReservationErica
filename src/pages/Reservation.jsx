43826c5fab137ecc77dc4c08e39c6e8695b87054
hajoon <gkwns0516@gmail.com>
2023-10-01 01:14:28 +0900

    [ADD] date picker 라이브러리 설치 및 초기 설정 세팅

--- src/pages/Reservation.jsx
+++ src/pages/Reservation.jsx
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { BsCalendar4Event } from 'react-icons/bs';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { addDays } from 'date-fns';
+import { ko } from 'date-fns/esm/locale';
 
 export default function Reservation() {
   const params = useParams();
-
   console.log(params);
+  const [startDate, setStartDate] = useState(null);
+
   return (
     <div className='flex flex-col w-full h-full'>
       <section className='bg-gray4 p-4 flex flex-col gap-2'>
         <p className='text-xl font-extrabold'>Smash Room {params.id}번 방</p>
         <p className='text-sm font-thin'>{params.id}번 방 상세정보 ...</p>
       </section>
-      <section>
-        <div className='flex'>
+      <section className='py-2 px-4'>
+        <div className='flex items-center gap-2 font-bold'>
           <BsCalendar4Event size={18} />
-          <p className=''>예약할 날짜를 입력해주세요</p>
+          <p className=''>예약할 날짜를 선택해주세요</p>
         </div>
-        <div>달력들어갈부분</div>
+        <div>
+          <DatePicker
+            locale={ko}
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+            minDate={new Date()}
+            maxDate={addDays(new Date(), 7)}
+            placeholderText='최대 7일만 고를 수 있음'
+          />
+        </div>
       </section>
     </div>
   );
