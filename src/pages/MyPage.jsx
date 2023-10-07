e36da070d23e79bdb18b31a3f745f28fdc155340
Yoonjit <jisu9083@naver.com>
2023-10-07 13:38:41 +0900

    [ADD] 예약 전/예약 후 분리

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -6,10 +6,11 @@ import PlaceCard from "../components/PlaceCard.jsx";
 import MypageCard from "../components/MypageCard.jsx";
 
 function MyPage() {
+  let reserve = true; // true: 예약 전, false: 예약 후(이용 전, 이용 후)
+
   return (
     <div className="px-4 rounded-b-lg rounded-2xl">
       <Profile />
-
       <div>
         <p
           className="semibold"
@@ -18,8 +19,17 @@ function MyPage() {
           이용 및 예약 현황
         </p>
       </div>
-
-      <MypageCard />
+      {reserve ? (
+        <div className="w-full flex flex-col bg-gray4 border-[1px] rounded-t-2xl rounded-b-2xl border-[1px] mb-80">
+          <section className="w-full flex flex-col bg-gray4 rounded-t-2xl rounded-b-2xl px-4 py-20 text-gray1">
+            <p style={{ display: "flex", justifyContent: "center" }}>
+              현재 이용 및 예약 현황이 없습니다.
+            </p>
+          </section>
+        </div>
+      ) : (
+        <MypageCard />
+      )}
     </div>
   );
 }
