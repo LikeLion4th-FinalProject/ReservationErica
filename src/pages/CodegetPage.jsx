955be1a2e7c4d4a41a8bf1ceb566ec0eec0840fd
bishoe01 <bishoe01@gmail.com>
2023-10-10 15:04:08 +0900

    [ADD] 유저정보 받아오기 closes#12

--- src/pages/CodegetPage.jsx
+++ src/pages/CodegetPage.jsx
@@ -1,37 +1,67 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Rest_api_key, redirect_uri } from "../static";
 
 function CodegetPage() {
   const [userData, setUserData] = useState(null);
+  const [userToken, setUserToken] = useState(null);
   const code = new URL(window.location.href).searchParams.get("code");
 
-  const getKaKaoUserData = async (token) => {
+  const getKaKaoToken = async () => {
     try {
-      const kakaoUser = await axios.get(`https://kapi.kakao.com/v2/user/me`, {
+      const res = await axios.post(
+        "https://kauth.kakao.com/oauth/token",
+        {
+          grant_type: "authorization_code",
+          client_id: Rest_api_key,
+          redirect_uri: redirect_uri,
+          code: code,
+        },
+        {
+          headers: {
+            "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+          },
+        }
+      );
+      setUserToken(res.data.access_token);
+    } catch (error) {
+      console.error("Error fetching access token: ", error.response.data);
+    }
+  };
+
+  const getKaKaoUserData = async (access_token) => {
+    try {
+      const res = await axios.get("https://kapi.kakao.com/v2/user/me", {
         headers: {
-          Authorization: `Bearer ${token}`,
+          Authorization: `Bearer ${access_token}`,
         },
       });
-      return kakaoUser.data;
+      setUserData(res.data);
+      console.log("User Data: ", res.data);
     } catch (error) {
-      console.error("Failed to fetch user data", error);
-      return null;
+      console.error("Error fetching user data: ", error.response.data);
     }
   };
 
   useEffect(() => {
-    const fetchUserData = async () => {
-      const data = await getKaKaoUserData(code);
-      setUserData(data);
-    };
-
-    fetchUserData();
+    if (code) {
+      getKaKaoToken();
+    }
   }, [code]);
+
+  useEffect(() => {
+    if (userToken) {
+      getKaKaoUserData(userToken);
+    }
+  }, [userToken]);
 
   return (
     <div>
-      {code && <p>Code: {code}</p>}
-      {userData && <p>User Data: {JSON.stringify(userData)}</p>}
+      {userData && (
+        <>
+          <h2>Welcome, {userData.properties.nickname}!</h2>
+        </>
+      )}
     </div>
   );
 }
