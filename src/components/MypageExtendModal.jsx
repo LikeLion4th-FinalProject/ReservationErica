ba60bc0708f1e73183c924113418c8bcf8f70393
Yoonjit <jisu9083@naver.com>
2023-10-15 23:33:01 +0900

    [ADD] 마이페이지 취소/연장 모달 추가

---
+++ src/components/MypageExtendModal.jsx
@@ -0,0 +1,35 @@
+import "../index.css";
+import "../App.css";
+
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function MypageCancelModal({ content, extendCount, isOpen }) {
+  const navigate = useNavigate();
+
+  return (
+    <div className='fixed top-0 left-0 flex justify-center items-center w-full h-full z-[99]'>
+      <div className='w-full h-full bg-black opacity-50'></div>
+      <div className='absolute max-w-[400px] w-[80%] min-h-[150px] h-[20%] bg-white shadow-xl flex flex-col justify-between rounded-3xl'>
+        <h1 className='flex justify-center items-center h-full bg-gray3 rounded-t-3xl'>
+          {content}
+        </h1>
+
+        <div className='w-full h-1/3 flex justify-between items-center'>
+          <button
+            onClick={() => isOpen(false)}
+            className='w-1/2 h-full text-center'
+          >
+            아니오
+          </button>
+          <button
+            onClick={() => isOpen(false)}
+            className='w-1/2 h-full text-center bg-[#0D51FF] rounded-br-3xl text-white '
+          >
+            예
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/SelectMenu2.jsx
+++ src/components/SelectMenu2.jsx
@@ -1,20 +1,69 @@
+import { useEffect, useState } from "react";
+import MypageCancelModal from "./MypageCancelModal";
+import MypageExtendModal from "./MypageCancelModal";
 import "../index.css";
 import "../App.css";
+import { render } from "react-dom";
 
 function SelectMenu2() {
+  const [isCancelModalOpen, setCancelModalOpen] = useState(false);
+  const [isExtendModalOpen, setExtendModalOpen] = useState(false);
+
   return (
     <div
       className="bg-gray3 flex items-center grid grid-flow-col justify-stretch rounded-b-lg"
       style={{ fontSize: "14px" }}
     >
-      <button className="flex items-center justify-center py-3 border-r border-gray-300">
-        <h1>취소하기</h1>
-      </button>
-      <button className="flex items-center justify-center py-3 text-white bg-gray0 rounded-br-lg border-gray-300 border-b border-gray0">
-        <h1>연장하기</h1>
-      </button>
+      {Button()}
+      {isCancelModalOpen && (
+        <MypageCancelModal
+          content={`이용 및 예약을 취소하시겠습니까?`}
+          isOpen={setCancelModalOpen}
+        />
+      )}
+      {isExtendModalOpen && (
+        <MypageExtendModal
+          content={`시간을 연장하시겠습니까?`}
+          extendCount={2}
+          isOpen={setExtendModalOpen}
+        />
+      )}
     </div>
   );
+
+  function Button() {
+    const [isFormValid, setFormValid] = useState(true);
+
+    if (isFormValid) {
+      return (
+        <>
+          <button
+            onClick={() => setCancelModalOpen(true)}
+            className="flex items-center justify-center py-3 border-r border-gray-300"
+          >
+            <h1>취소하기</h1>
+          </button>
+          <button
+            onClick={() => setExtendModalOpen(true)}
+            className="flex items-center justify-center py-3 text-white bg-gray0 rounded-br-lg border-gray-300 border-b border-gray0"
+          >
+            <h1>연장하기</h1>
+          </button>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <button className="flex items-center justify-center py-3 border-r border-gray-300">
+            <h1>취소하기</h1>
+          </button>
+          <button className="flex items-center justify-center py-3 text-white bg-gray0 rounded-br-lg border-gray-300 border-b border-gray0">
+            <h1>연장하기</h1>
+          </button>
+        </>
+      );
+    }
+  }
 }
 
 export default SelectMenu2;
