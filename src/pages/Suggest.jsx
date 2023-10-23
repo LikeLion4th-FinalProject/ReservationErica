e4ab60f2b382dbc28183884075224885b8dbd12f
Yoonjit <jisu9083@naver.com>
2023-10-23 18:28:16 +0900

    [FIX] 건의하기 완성 (textarea-focus 오류 해결 X)

--- src/pages/Suggest.jsx
+++ src/pages/Suggest.jsx
@@ -2,12 +2,15 @@ import "../index.css";
 import "../App.css";
 
 import React from "react";
-import { useState } from "react";
+import { render } from "react-dom";
+import { useEffect, useState } from "react";
 
 import SuggestDropdown from "../components/Suggest/SuggestDropdown";
+import SuggestModal from "../components/Suggest/SuggestModal";
 
 function Suggest() {
   const [selectedSuggest, setSelectedSuggest] = useState(null);
+  const [isSuggestModalOpen, setSuggestModalOpen] = useState(false);
 
   // textarea 최대 글자수 제한
   const [text, setText] = useState("");
@@ -22,6 +25,9 @@ function Suggest() {
 
   // textarea 입력 여부에 따라 버튼 색상 결정
   const buttonColor = text.length > 0 ? "#0D51FF" : "#CCCCCC";
+
+  // selectedSuggest가 null인 경우 textarea 비활성화
+  const isTextareaDisabled = selectedSuggest === null;
 
   // textarea 입력 여부에 따라 버튼 활성화/비활성화 결정
   const isButtonDisabled = text.length === 0;
@@ -66,10 +72,13 @@ function Suggest() {
       />
       <div className="px-5">
         <textarea
-          className="rounded-2xl bg-gray4 text-sm"
+          className={`rounded-2xl bg-gray4 text-sm ${
+            isTextareaDisabled ? "" : "focus:border-blue-700" // 이거 왜 안 바뀌지?
+          }`}
           placeholder="자세한 내용을 적어주세요"
           value={text}
           onChange={handleChange}
+          disabled={isTextareaDisabled} // textarea 비활성화 설정: 건의사유 선택 안 했을 때
           style={{
             width: "100%",
             height: "180px",
@@ -104,8 +113,51 @@ function Suggest() {
           건의하기
         </p>
       </button>
+
+      <div
+        className="bg-gray3 flex items-center grid grid-flow-col justify-stretch rounded-b-lg"
+        style={{ fontSize: "14px" }}
+      >
+        {Button()}
+        {isSuggestModalOpen && (
+          <SuggestModal
+            content1={`건의하시겠습니까?`}
+            content2={`Smash 1`}
+            content3={`2023-09-26`}
+            content4={selectedSuggest}
+            isOpen={setSuggestModalOpen}
+          />
+        )}
+      </div>
     </div>
   );
+
+  function Button() {
+    const [isFormValid, setFormValid] = useState(true);
+
+    if (isFormValid) {
+      return (
+        <>
+          <button
+            disabled={isButtonDisabled}
+            className={`fixed bottom-0 w-full h-16 flex justify-around items-center ${
+              isButtonDisabled ? "bg-gray-300" : "bg-blue-600"
+            }`}
+            style={{ zIndex: 9999 }}
+            onClick={() => setSuggestModalOpen(true)}
+          >
+            <p
+              className={`${isButtonDisabled ? "text-gray-500" : "text-white"}`}
+            >
+              건의하기
+            </p>
+          </button>
+        </>
+      );
+    } else {
+      return <></>;
+    }
+  }
 }
 
 export default Suggest;
