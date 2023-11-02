ee545d6299f4b5a340dbbb9216acea65d1a010a0
hajoon <gkwns0516@gmail.com>
2023-11-03 03:46:43 +0900

    [ADD] 예약확인 및 확정 컴포넌트 재구조화 + 퍼블리싱

--- src/components/modal/ConfirmModal.jsx
+++ src/components/modal/ConfirmModal.jsx
@@ -2,13 +2,14 @@ import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { reserveTime } from '../../styles/static';
 import { client } from '../../api/client';
-import { reserveInfomation } from '../../pages/ReserveHome';
+import { reserveConfirm } from '../../pages/ReserveHome';
+import ConfirmInfo from '../reservation/ConfirmInfo';
 
 export default function ConfirmModal({ content, isOpen, reserveInfo }) {
   const navigate = useNavigate();
 
-  const isCompleteReserve = useContext(reserveInfomation);
-  const reserveConfirm = () => {
+  const isCompleteReserve = useContext(reserveConfirm);
+  const handleReserveConfirm = () => {
     client
       .post('roomreserve/', reserveInfo)
       .then((response) => console.log(response.data))
@@ -16,41 +17,19 @@ export default function ConfirmModal({ content, isOpen, reserveInfo }) {
     // navigate('/mypage/0');
     isCompleteReserve(true);
   };
-  console.log(reserveInfo);
+  console.log('해당 예약 정보로 예약하실 겁니까? : ', reserveInfo);
 
   return (
     <div className='fixed top-0 left-0 flex justify-center items-center w-full h-full z-[99]'>
       <div className='w-full h-full bg-black opacity-50'></div>
       <div className='absolute max-w-[400px] w-[80%]  bg-white shadow-xl flex flex-col justify-between rounded-3xl'>
-        <h1 className='flex justify-center items-center h-full bg-gray3 rounded-t-3xl py-8'>
-          {reserveInfo ? (
-            <>
-              <div>
-                <p className='font-bold text-lg text-center mb-[18px]'>
-                  예약하시겠습니까?
-                </p>
-                <div className='flex flex-col'>
-                  <p className='flex gap-10'>
-                    <span className='text-gray-500 font-thin'>이름</span>
-                    <span>{content}</span>
-                  </p>
-                  <p className='flex gap-10'>
-                    <span className='text-gray-500 font-thin'>날짜</span>
-                    <span>{reserveInfo.date}</span>
-                  </p>
-                  <p className='flex gap-10'>
-                    <span className='text-gray-500 font-thin'>시간</span>
-                    <span>{`${reserveTime[reserveInfo.start]} ~ ${
-                      reserveTime[reserveInfo.end]
-                    }`}</span>
-                  </p>
-                </div>
-              </div>
-            </>
-          ) : (
-            { content }
-          )}
-        </h1>
+        {reserveInfo ? (
+          <>
+            <ConfirmInfo content={'예약하시겠습니까?'} resInfo={reserveInfo} />
+          </>
+        ) : (
+          { content }
+        )}
         <div className='w-full flex justify-between items-center'>
           <button
             onClick={() => isOpen(false)}
@@ -59,7 +38,7 @@ export default function ConfirmModal({ content, isOpen, reserveInfo }) {
             아니오
           </button>
           <button
-            onClick={reserveConfirm}
+            onClick={handleReserveConfirm}
             className='w-1/2 h-full text-center bg-[#0D51FF] rounded-br-3xl text-white  py-4'
           >
             예
