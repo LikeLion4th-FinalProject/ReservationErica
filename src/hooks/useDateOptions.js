d0b54fd9fcd26e7daa645f00ad183d827d1bae13
hajoon <gkwns0516@gmail.com>
2023-11-03 03:44:17 +0900

    [FIX] 00:00시 지나면 offset값이 안먹힘.. 뭐지

--- src/hooks/useDateOptions.js
+++ src/hooks/useDateOptions.js
@@ -4,14 +4,14 @@ import { dayList } from '../styles/static';
 function useDateOptions() {
   const [options, setOptions] = useState([]);
 
-  // const offset = new Date().getTimezoneOffset() * 60000; // ms 단위를 맞추기 위해 60000 곱해줌
-  const today = new Date();
-  // const today = new Date(Date.now() - offset);
+  const offset = new Date().getTimezoneOffset() * 60000; // ms 단위를 맞추기 위해 60000 곱해줌
+  // const today = new Date();
+  const today = new Date(Date.now() - offset);
   // console.log(today);
   useEffect(() => {
     const tempOptions = [];
     for (let i = 0; i < 7; i++) {
-      const date = new Date(Date.now());
+      const date = new Date(Date.now() - offset);
       date.setDate(today.getDate() + i);
       // console.log(date);
       const formattedDate = date.toISOString().split('T')[0];
