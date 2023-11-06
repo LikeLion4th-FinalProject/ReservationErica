8cd3099271c67357ff5bc04b9899b34171ca3380
hajoon <gkwns0516@gmail.com>
2023-11-06 14:19:19 +0900

    [FIX] 예약/마이페이지 접근권한 재설정

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,19 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { RouterProvider } from "react-router";
-import { createBrowserRouter } from "react-router-dom";
-import Home from "./pages/Home.jsx";
-import LoginPage from "./pages/LoginPage.jsx";
-import CodegetPage from "./pages/CodegetPage.jsx";
-import Reservation from './pages/Reservation.jsx';
-import ReserveHome from "./pages/ReserveHome.jsx";
-import MyPage from "./pages/MyPage.jsx";
-import BeforeSuggest from "./pages/BeforeSuggest.jsx";
-import BeforeSuggestDetail from "./pages/BeforeSuggestDetail.jsx";
-import BeforeReserve from "./pages/BeforeReserve.jsx";
-import Suggest from "./pages/Suggest.jsx";
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { RouterProvider } from 'react-router';
+import { createBrowserRouter } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import CodegetPage from './pages/CodegetPage.jsx';
+import ReserveHome from './pages/ReserveHome.jsx';
+import MyPage from './pages/MyPage.jsx';
+import BeforeSuggest from './pages/BeforeSuggest.jsx';
+import BeforeSuggestDetail from './pages/BeforeSuggestDetail.jsx';
+import BeforeReserve from './pages/BeforeReserve.jsx';
+import Suggest from './pages/Suggest.jsx';
 import SignupPage from './pages/SignupPage.jsx';
-import { PrivateRoute } from "./hooks/PrivateRoute.jsx";
+import { PrivateRoute } from './hooks/PrivateRoute.jsx';
 import ReserveRecord from './pages/ReserveRecord.jsx';
 
 const router = createBrowserRouter([
@@ -39,35 +38,35 @@ const router = createBrowserRouter([
         element: <CodegetPage />,
       },
       {
-        path: "reservation/:id",
+        path: 'reservation',
         element: (
           <PrivateRoute>
-            <Reservation />
+            <ReserveHome />
           </PrivateRoute>
         ),
       },
       {
-        path: 'reservation',
-        element: <ReserveHome />,
+        path: 'mypage/:id',
+        element: (
+          <PrivateRoute>
+            <MyPage />,
+          </PrivateRoute>
+        ),
       },
       {
-        path: "mypage/:id",
-        element: <MyPage />,
-      },
-      {
-        path: "mypage/before-suggest",
+        path: 'mypage/before-suggest',
         element: <BeforeSuggest />,
       },
       {
-        path: "mypage/before-reserve",
+        path: 'mypage/before-reserve',
         element: <BeforeReserve />,
       },
       {
-        path: "mypage/before-suggest-detail",
+        path: 'mypage/before-suggest-detail',
         element: <BeforeSuggestDetail />,
       },
       {
-        path: "mypage/suggest",
+        path: 'mypage/suggest',
         element: <Suggest />,
       },
       {
