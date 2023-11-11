137d80e5f866b84ead1d22ba248f1549f2679cc0
hajoon <gkwns0516@gmail.com>
2023-11-12 00:41:38 +0900

    [FIX] 날짜 offest 조정 드디어 끝 .. 이제 알았다, toISOstring을 할 때만 영국 현지시간으로 바뀐다는 것을 ㅡㅡ

--- src/hooks/useDateOptions.js
+++ src/hooks/useDateOptions.js
@@ -5,15 +5,15 @@ function useDateOptions() {
   const [options, setOptions] = useState([]);
 
   const offset = new Date().getTimezoneOffset() * 60000; // ms 단위를 맞추기 위해 60000 곱해줌
-  // const today = new Date();
-  const today = new Date(Date.now() - offset);
-  console.log('today: ', today);
+  // const today = new Date(Date.now() - offset);
+  const today = new Date();
+  // console.log('today: ', today);
   useEffect(() => {
     const tempOptions = [];
     for (let i = 0; i < 7; i++) {
       const date = new Date(Date.now() - offset);
       date.setDate(today.getDate() + i);
-      // console.log(date);
+      // console.log('date : ', date);
       const formattedDate = date.toISOString().split('T')[0];
       // console.log(formattedDate);
       tempOptions.push({
