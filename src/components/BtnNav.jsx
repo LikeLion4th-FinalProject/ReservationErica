6b77cd267f52fc7feb4a66b6e57d93e625109abc
hajoon <gkwns0516@gmail.com>
2023-11-29 15:10:16 +0900

    [ADD] mypage 동적라우팅 추가
    이렇게 하는거 맞나 ㅠ

--- src/components/BtnNav.jsx
+++ src/components/BtnNav.jsx
@@ -1,28 +1,40 @@
-import { AiOutlineUser, AiOutlineCheck } from "react-icons/ai";
-import { BiSolidUser } from "react-icons/bi";
-import { Link, useLocation } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import { AiOutlineUser, AiOutlineCheck } from 'react-icons/ai';
+import { BiSolidUser } from 'react-icons/bi';
+import { Link, useLocation } from 'react-router-dom';
+import { getUserId } from '../api/mypage';
 
 function BtnNav() {
   const location = useLocation();
 
-  const isReservationPage = location.pathname.startsWith("/reservation");
-  const isMyPage = location.pathname.includes("mypage");
+  const isReservationPage = location.pathname.startsWith('/reservation');
+  const isMyPage = location.pathname.includes('mypage');
 
   let visible = true;
 
   switch (location.pathname) {
-    case "/mypage/before-suggest":
+    case '/mypage/before-suggest':
       visible = false;
       break;
-    case "/mypage/before-reserve":
+    case '/mypage/before-reserve':
       visible = false;
       break;
-    case "/mypage/suggest":
+    case '/mypage/suggest':
       visible = false;
       break;
     default:
       visible = true;
   }
+
+  const [userId, setUserId] = useState();
+
+  const fetchUserId = async () => {
+    setUserId(await getUserId());
+  };
+
+  useEffect(() => {
+    fetchUserId();
+  }, []);
 
   return (
     <>
@@ -31,27 +43,27 @@ function BtnNav() {
           className={`sticky bottom-0 left-0 right-0 z-50 flex justify-around items-center  w-full h-[50px]`}
         >
           <Link
-            to={"/reservation"}
+            to={'/reservation'}
             className={`flex items-center flex-col justify-center w-full h-full ${
-              isReservationPage ? " text-myblue" : "bg-white text-gray0"
+              isReservationPage ? ' text-myblue' : 'bg-white text-gray0'
             }`}
           >
-            <AiOutlineCheck className="text-lg" size={23} />
-            <span className="text-btn">예약하기</span>
+            <AiOutlineCheck className='text-lg' size={23} />
+            <span className='text-btn'>예약하기</span>
           </Link>
           <Link
-            to={"/mypage/:id"}
+            to={`/mypage/${userId}`}
             className={`flex items-center flex-col justify-center w-full h-[50px]`}
           >
             {isMyPage ? (
               <>
-                <BiSolidUser className="text-lg text-myblue" size={23} />
-                <span className="text-btn text-myblue">마이페이지</span>
+                <BiSolidUser className='text-lg text-myblue' size={23} />
+                <span className='text-btn text-myblue'>마이페이지</span>
               </>
             ) : (
               <>
-                <AiOutlineUser className="text-lg text-gray0" size={23} />
-                <span className="text-btn">마이페이지</span>
+                <AiOutlineUser className='text-lg text-gray0' size={23} />
+                <span className='text-btn'>마이페이지</span>
               </>
             )}
           </Link>
